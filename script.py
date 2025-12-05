d33a672481d0bf1f7aca13aa1a3b1b7aea669217
olia <olia7nr@gmail.com>
2025-12-06 02:06:59 +0330

    bug

--- script.py
+++ script.py
@@ -1,20 +1,20 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-TOKEN = "توکن_بات_اینجا"
+TOKEN = "8405267225:AAEZR3vOM7tKTMf7IJyG-vES0SoFh5ywtTM"
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     name = update.message.text
-
     # ذخیره اسم در فایل
     with open("names.txt", "a", encoding="utf-8") as f:
         f.write(name + "\n")
-
     await update.message.reply_text(f"{name} عزیز، خوش اومدی!")
 
+# ساخت بات با ApplicationBuilder
 app = ApplicationBuilder().token(TOKEN).build()
 
+# اضافه کردن handler برای پیام‌های متنی
 app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), reply))
 
+# اجرای بات (polling)
 app.run_polling()
-
